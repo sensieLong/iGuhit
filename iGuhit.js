48c1cb579ade38fec3744f2db6ae6332a4077236
sensieLong <111825107+sensieLong@users.noreply.github.com>
2024-07-02 13:19:40 +0800

    move the security to onmousedown event

--- iGuhit.js
+++ iGuhit.js
@@ -70,23 +70,6 @@ $(function () {
 });
 // end of jquery
 
-// security set up github.io
-var headCheck = window.location.href;
-var headCheckString = headCheck.toString();
-var headCheckSimplify = headCheckString.substring(0, 24);
-var headCheckSafari = headCheckString.substring(0, 23);
-var pass = 'https://iguhit.enums.org';
-var safari = 'http://iguhit.enums.org';
-// check internet connection first
-checkInternetConnection();
-// check if the app is not in other domain, alert app not genuin if so
-if (headCheckSimplify == pass || headCheckSafari == safari) {
-    alert('Thank you for using enums.org');
-} else {
-    return alert('app not genuin'), location.reload();
-};
-// end of verification
-
 // paper section
 paper.install(window);
 var raster, rasterColorHolder;
@@ -674,6 +657,23 @@ function findHandle(point) {
 }
 // mouseDown section
 function onMouseDown(event) {
+    // security set up github.io
+    var headCheck = window.location.href;
+    var headCheckString = headCheck.toString();
+    var headCheckSimplify = headCheckString.substring(0, 24);
+    var headCheckSafari = headCheckString.substring(0, 23);
+    var pass = 'https://iguhit.enums.org';
+    var safari = 'http://iguhit.enums.org';
+    // check internet connection first
+    checkInternetConnection();
+    // check if the app is not in other domain, alert app not genuin if so
+    if (headCheckSimplify == pass || headCheckSafari == safari) {
+        alert('Thank you for using enums.org');
+    } else {
+        return alert('app not genuin'), location.reload();
+    };
+    // end of verification
+    
     // freepen section
     if (changeTool === 'pen') {
         tool.onKeyDown = function (event) {
